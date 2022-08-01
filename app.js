685f8b8377e0ae71a15c0f05b4f4e8a5c9324822
cuong2312 <cuongnguyenbagi@gmail.com>
2022-08-01 16:05:18 +0700

    update

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-import  Express from 'express';
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import routerProduct from './src/routers/product';
+import routerAuth from './src/routers/auth';
+import routerCategory from './src/routers/category'
+import mongoose from 'mongoose';
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", routerProduct);
+app.use("/api", routerAuth);
+app.use("/api", routetCategory);
+
+mongoose.connect('mongodb://localhost:27017/', () => {
+    console.log('successfully');
+}
+);
+
+app.listen(process.env.PORT, () => {
+    console.log('Kết nối thành công, cổng' + process.env.PORT)
+})
